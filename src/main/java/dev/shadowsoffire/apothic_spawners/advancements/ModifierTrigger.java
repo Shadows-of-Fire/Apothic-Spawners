f16df7082fe85775dc1d0e1c1450bb9a21c2b22b
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-03-12 21:26:57 -0700

    Fix ModifierTrigger (sorta)

--- src/main/java/dev/shadowsoffire/apothic_spawners/advancements/ModifierTrigger.java
+++ src/main/java/dev/shadowsoffire/apothic_spawners/advancements/ModifierTrigger.java
@@ -8,6 +8,8 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.shadowsoffire.apothic_spawners.block.ApothSpawnerTile;
 import dev.shadowsoffire.apothic_spawners.block.ApothSpawnerTile.SpawnerLogicExt;
 import dev.shadowsoffire.apothic_spawners.modifiers.SpawnerModifier;
+import dev.shadowsoffire.apothic_spawners.modifiers.SpawnerStat;
+import dev.shadowsoffire.apothic_spawners.stats.SpawnerStats;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.advancements.critereon.MinMaxBounds.Ints;
@@ -26,6 +28,7 @@ public class ModifierTrigger extends SimpleCriterionTrigger<ModifierTrigger.Trig
         this.trigger(player, inst -> inst.test(player, tile, modif));
     }
 
+    // TODO: Fix this, it needs to be able to parse all stats and compare them against the values.  Problem is mapping the stats to bounds objects.
     public static record TriggerInstance(Optional<ContextAwarePredicate> player, Ints minDelay, Ints maxDelay, Ints spawnCount, Ints nearbyEnts,
         Ints playerRange, Ints spawnRange, Optional<Boolean> ignorePlayers, Optional<Boolean> ignoreConditions, Optional<Boolean> redstone,
         Optional<Boolean> ignoreLight, Optional<Boolean> noAI, Optional<Boolean> silent, Optional<Boolean> baby) implements SimpleCriterionTrigger.SimpleInstance {
@@ -46,21 +49,28 @@ public class ModifierTrigger extends SimpleCriterionTrigger<ModifierTrigger.Trig
                 ExtraCodecs.strictOptionalField(Codec.BOOL, "no_ai").forGetter(TriggerInstance::noAI),
                 ExtraCodecs.strictOptionalField(Codec.BOOL, "silent").forGetter(TriggerInstance::silent),
                 ExtraCodecs.strictOptionalField(Codec.BOOL, "baby").forGetter(TriggerInstance::baby))
-            .apply(inst, TriggerInstance::new));    
+            .apply(inst, TriggerInstance::new));
 
         public boolean test(ServerPlayer player, ApothSpawnerTile tile, SpawnerModifier modif) {
             SpawnerLogicExt logic = (SpawnerLogicExt) tile.spawner;
-            if (!this.minDelay.matches(logic.minSpawnDelay) || !this.maxDelay.matches(logic.maxSpawnDelay) || !this.spawnCount.matches(logic.spawnCount) || !this.nearbyEnts.matches(logic.maxNearbyEntities)) return false;
+            if (!this.minDelay.matches(logic.minSpawnDelay)) return false;
+            if (!this.maxDelay.matches(logic.maxSpawnDelay)) return false;
+            if (!this.spawnCount.matches(logic.spawnCount)) return false;
+            if (!this.nearbyEnts.matches(logic.maxNearbyEntities)) return false;
             if (!this.playerRange.matches(logic.requiredPlayerRange)) return false;
             if (!this.spawnRange.matches(logic.spawnRange)) return false;
-            if (this.ignorePlayers.isPresent() && tile.ignoresPlayers != this.ignorePlayers.get()) return false;
-            if (this.ignoreConditions.isPresent() && tile.ignoresConditions != this.ignoreConditions.get()) return false;
-            if (this.redstone.isPresent() && tile.redstoneControl != this.redstone.get()) return false;
-            if (this.ignoreLight.isPresent() && tile.ignoresLight != this.ignoreLight.get()) return false;
-            if (this.noAI.isPresent() && tile.hasNoAI != this.noAI.get()) return false;
-            if (this.silent.isPresent() && tile.silent != this.silent.get()) return false;
-            if (this.baby.isPresent() && tile.baby != this.baby.get()) return false;
+            if (check(tile, SpawnerStats.IGNORE_PLAYERS, this.ignorePlayers)) return false;
+            if (check(tile, SpawnerStats.IGNORE_CONDITIONS, this.ignoreConditions)) return false;
+            if (check(tile, SpawnerStats.REDSTONE_CONTROL, this.redstone)) return false;
+            if (check(tile, SpawnerStats.IGNORE_LIGHT, this.ignoreLight)) return false;
+            if (check(tile, SpawnerStats.NO_AI, this.noAI)) return false;
+            if (check(tile, SpawnerStats.SILENT, this.silent)) return false;
+            if (check(tile, SpawnerStats.BABY, this.baby)) return false;
             return true;
+        }
+
+        private <T> boolean check(ApothSpawnerTile tile, SpawnerStat<T> stat, Optional<T> target) {
+            return target.isEmpty() || target.get() == stat.getValue(tile);
         }
 
     }
